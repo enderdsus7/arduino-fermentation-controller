59bbed04ac07f7564d0e7f17fcaddd0a13a7be3c
Trent Jacobs <trent.jacobs@gmail.com>
2009-10-22 23:21:01 -0500

    Added basic logging support via SD card over SPI interface
    Need to improve button response, look into interrupts for button detection.
    
    Need to modify uFat library to reduce size and eliminate unnecessary debugging,
    like statusing the serial I/O on an SD write

--- applet/thermistor_lcd.cpp
+++ applet/thermistor_lcd.cpp
@@ -2,17 +2,19 @@
   // pin 1 to 5V, 2 to analog input, 3 to GND.
   
   #include <SoftwareSerial.h>
+  #include "SDuFAT.h"
   
-  #define TEMP1 1
+  #define TEMP1 1 // analog pin 1
   #define rxPin 4 // any unused pin number is fine
-  #define BUTTON 6
+  #define BUTTON 6 // digital pin 6
   #define transistor 7 // output for transistor switch
   #define txPin 14 // analog pin 0
   
   #include "WProgram.h"
-void setup();
 void printTTY();
+void printSD();
 void printLCD();
+void setup();
 void loop();
 SoftwareSerial mySerial = SoftwareSerial(rxPin, txPin);
   
@@ -20,14 +22,54 @@ SoftwareSerial mySerial = SoftwareSerial(rxPin, txPin);
   int samples[8];
   int maxtemp = -1000;
   int mintemp = 1000;
+  unsigned long oldtime;
   int i = 0;
   
   int val = 0;
   int old_val = 0;
-  int state = 0;
+  int state = 1;
   
   unsigned long startTime;
   unsigned long lastTime;
+  
+  char logstring[30];
+  int result=0;
+  
+  void printTTY(){
+    Serial.print(tempf,DEC);
+    Serial.print(" Fahrenheit -> ");
+    Serial.print(maxtemp,DEC);
+    Serial.print(" Max, ");
+    Serial.print(mintemp,DEC);
+    Serial.println(" Min ");
+  }
+  
+  void printSD(){
+    sprintf(logstring,"%d,%lu",tempf,lastTime);
+    result = SD.println("hola.txt",logstring);
+  }
+  
+  void printLCD(){
+      mySerial.print("?x00?y0");
+      mySerial.print("CUR   MIN   MAX");
+  
+      mySerial.print("?x00?y1");
+      mySerial.print(tempf/10,DEC);
+      mySerial.print(".");
+      mySerial.print(tempf%10,DEC);
+  
+      mySerial.print("?t");
+      
+      mySerial.print(mintemp/10,DEC);
+      mySerial.print(".");
+      mySerial.print(mintemp%10,DEC);
+    
+      mySerial.print("?t");
+      
+      mySerial.print(maxtemp/10,DEC);
+      mySerial.print(".");
+      mySerial.print(maxtemp%10,DEC);
+  }
   
   void setup(){
     startTime = millis();
@@ -50,49 +92,16 @@ SoftwareSerial mySerial = SoftwareSerial(rxPin, txPin);
     analogReference(INTERNAL); // set A/D to use 1.1V reference instead of 5V for better accuracy
   }
   
-  void printTTY(){
-    Serial.print(tempf,DEC);
-    Serial.print(" Fahrenheit -> ");
-    Serial.print(maxtemp,DEC);
-    Serial.print(" Max, ");
-    Serial.print(mintemp,DEC);
-    Serial.println(" Min ");
-  }
-  
-  void printLCD(){
-    if(millis()-lastTime > 1000){
-      mySerial.print("?x00?y0");
-      mySerial.print("CUR   MIN   MAX");
-  
-      mySerial.print("?x00?y1");
-      mySerial.print(tempf/10,DEC);
-      mySerial.print(".");
-      mySerial.print(tempf%10,DEC);
-  
-      mySerial.print("?t");
-      
-      mySerial.print(mintemp/10,DEC);
-      mySerial.print(".");
-      mySerial.print(mintemp%10,DEC);
-    
-      mySerial.print("?t");
-      
-      mySerial.print(maxtemp/10,DEC);
-      mySerial.print(".");
-      mySerial.print(maxtemp%10,DEC);
-      
-      lastTime = millis();
-    }
-  }
-    
-  
   void loop(){
-    for(i = 0; i < 8; i++){
-      samples[i] = ( analogRead(TEMP1) * 1.1 * 1000 ) / 1024; // temp in deg F
-      // temp is read out in 10mV/degree F.  We are using analog reference of 1.1V.
-      tempf = tempf + samples[i];
-  //    delay(250);
-    }
+      
+  if(millis()-250 >= lastTime) {    
+    samples[i] = ( analogRead(TEMP1) * 1.1 * 1000 ) / 1024; // temp in deg F
+    // temp is read out in 10mV/degree F.  We are using analog reference of 1.1V.
+    tempf = tempf + samples[i];
+    lastTime = millis();
+    i++;
+  //delay(250);
+  }
     
     val = digitalRead(BUTTON);
     
@@ -100,27 +109,28 @@ SoftwareSerial mySerial = SoftwareSerial(rxPin, txPin);
       state = 1 - state;
       delay(20);
     }
-    
-    tempf = tempf/8.0;
   
-    if(millis() > 6000) {
-    if(tempf > maxtemp){maxtemp = tempf;}
-    if(tempf < mintemp){mintemp = tempf;}
-  
-    printTTY();
-    printLCD();
-    }
-    
-    old_val = val;
-    
     if (state == 1) {
-       digitalWrite(transistor, HIGH);
+      digitalWrite(transistor, HIGH);
     } else {
       digitalWrite(transistor, LOW);
     }
     
-    tempf = 0;
-  
+    if(i>=8) {
+      tempf = tempf/(i);  
+      if(lastTime-startTime > 6000) {
+        if(tempf > maxtemp){maxtemp = tempf;}
+        if(tempf < mintemp){mintemp = tempf;}
+        printSD();
+        printLCD(); 
+      }
+      i=0;
+      tempf = 0;
+    }
+    
+    old_val = val;
+    
+    
   //  delay(500);
   }
 
